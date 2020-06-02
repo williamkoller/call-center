68266fa9027f7d7b08b0215d562dcdff7792f266
William Koller <wkoller25@gmail.com>
2020-06-02 14:50:20 -0300

    feat: add socket.io-client

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
 import './App.css';
 
+const socket = io('http://localhost:3001');
+
 function App() {
-  const [code, setCode] = useState(999999);
-  const [status, setstatus] = useState('waiting_call');
+  const [code, setCode] = useState(0);
+  const [status, setStatus] = useState('');
+  useEffect(() => {
+    socket.on('code', (code) => setCode(code));
+    socket.on('status', (status) => setStatus(status));
+  }, [])
   return (
     <div>
       <h2> { code } </h2>
